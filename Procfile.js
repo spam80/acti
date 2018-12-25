e6327dee7471481dc8566da03d3e8744f0692eb3
spam80 <45275318+spam80@users.noreply.github.com>
2018-12-25 11:53:05 +0100

    Add files via upload

---
+++ Procfile.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+var prefix = "$"
+
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag} !`);
+          client.user.setActivity("$help | $invite",{type: 'WATCHING'});
+  
+  });
+
+client.login(process.env.TOKEN);
